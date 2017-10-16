e22d29b9b12d554cdb112ca364c6bcd92afd1382
Bastien Chevallier <cheval_b0etna-alternance.net>
2017-10-16 02:48:00 +0200

    fix editor config

--- __mocks__/react.js
+++ __mocks__/react.js
@@ -3,7 +3,7 @@ const react = require("react");
 global.window = global;
 window.addEventListener = () => {};
 window.requestAnimationFrame = () => {
-	throw new Error("requestAnimationFrame is not supported in Node");
+  throw new Error("requestAnimationFrame is not supported in Node");
 };
 
 module.exports = react;
