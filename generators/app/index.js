d2f97517708b3eff1f522df5f1165806b8ea5567
Bastien Chevallier <cheval_b@etna-alternance.net>
2017-12-22 15:23:42 +0100

    wip

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,6 +6,27 @@ const yosay = require('yosay');
 const _ = require('lodash');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    // Argument
+    this.argument('name', { type: String, required: false });
+    this.argument('description', { type: String, required: false });
+    this.argument('homepage', { type: String, required: false });
+    this.argument('authorName', { type: String, required: false });
+    this.argument('authorEmail', { type: String, required: false });
+    this.argument('authorUrl', { type: String, required: false });
+    this.argument('keywords', { type: String, required: false });
+
+    // Option
+    this.option('skip-install');
+    this.option('i18n');
+    this.option('jest');
+    this.option('flow');
+    this.option('husky');
+    this.option('eslint');
+    this.option('browserSync');
+  }
   Prompting() {
     this.log(
       yosay(
@@ -64,42 +85,51 @@ module.exports = class extends Generator {
         type: 'confirm',
         name: 'i18n',
         message: 'Would you like to use i18n package?',
+        when: typeof this.options.i18n === 'undefined',
         default: true
       },
       {
         type: 'confirm',
         name: 'jest',
         message: 'Would you like to use Jest?',
+        when: typeof this.options.jest === 'undefined',
         default: true
       },
       {
         type: 'confirm',
         name: 'flow',
         message: 'Would you like to use Flow package (devOnly)?',
+        when: typeof this.options.flow === 'undefined',
         default: true
       },
       {
         type: 'confirm',
         name: 'husky',
         message: 'Would you like to use Husky (devOnly)?',
+        when: typeof this.options.husky === 'undefined',
         default: true
       },
       {
         type: 'confirm',
         name: 'eslint',
         message: 'Would you like to use Eslint (devOnly)?',
+        when: typeof this.options.eslint === 'undefined',
         default: true
       },
       {
         type: 'confirm',
         name: 'browserSync',
         message: 'Would you like to use BrowserSync (devOnly)?',
+        when: typeof this.options.browserSync === 'undefined',
         default: true
       }
     ];
 
-    return this.prompt(prompts).then(props => {
-      this.props = props;
+    return this.prompt(prompts).then(options => {
+      this.options = {
+        ...options,
+        ...this.options
+      };
     });
   }
 
@@ -112,6 +142,8 @@ module.exports = class extends Generator {
   }
 
   install() {
-    this.installDependencies();
+    if (!this.options['skip-install']) {
+      this.installDependencies();
+    }
   }
 };
